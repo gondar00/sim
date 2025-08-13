20a103c69dd91c813090aabd72919e9c08151acd
Gandharv <gandharvkumargarg@gmail.com>
2025-08-13 11:15:54 +0530

    hero|

--- apps/sim/app/(landing)/landing.tsx
+++ apps/sim/app/(landing)/landing.tsx
@@ -1,28 +1,38 @@
-'use client'
+"use client";
 
-import NavWrapper from './components/nav-wrapper'
-import Footer from './components/sections/footer'
-import Hero from './components/sections/hero'
-import Integrations from './components/sections/integrations'
-import Testimonials from './components/sections/testimonials'
+import NavWrapper from "./components/nav-wrapper";
+import Footer from "./components/sections/footer";
+import Hero from "./components/sections/hero";
+import Integrations from "./components/sections/integrations";
+import Testimonials from "./components/sections/testimonials";
 
 export default function Landing() {
   const handleOpenTypeformLink = () => {
-    window.open('https://form.typeform.com/to/jqCO12pF', '_blank')
-  }
+    window.open("https://form.typeform.com/to/jqCO12pF", "_blank");
+  };
 
   return (
-    <main className='relative min-h-screen bg-[#0C0C0C] font-geist-sans'>
-      <NavWrapper onOpenTypeformLink={handleOpenTypeformLink} />
+    <main className="relative min-h-screen bg-[#0C0C0C] font-geist-sans">
+      <h1 className="animation-container animate-fade-up font-semibold text-[42px] leading-[1.10] opacity-0 will-change-[opacity,transform] [animation-delay:200ms] md:text-[68px]">
+        Build / Deploy
+        <br />
+        Agent Workflows by Gandharv Garg
+      </h1>
 
-      <Hero />
-      <Testimonials />
+      <p className="animation-container mx-auto max-w-3xl animate-fade-up font-normal text-base text-neutral-400/80 leading-[1.5] tracking-normal opacity-0 will-change-[opacity,transform] [animation-delay:400ms] md:text-xl">
+        Launch agentic workflows with user-friendly environment for devs and
+        agents
+      </p>
+      {/* <NavWrapper onOpenTypeformLink={handleOpenTypeformLink} /> */}
+
+      {/* <Hero /> */}
+      {/* <Testimonials /> */}
       {/* <Features /> */}
-      <Integrations />
+      {/* <Integrations /> */}
       {/* <Blogs /> */}
 
       {/* Footer */}
-      <Footer onOpenTypeformLink={handleOpenTypeformLink} />
+      {/* <Footer onOpenTypeformLink={handleOpenTypeformLink} /> */}
     </main>
-  )
+  );
 }
